77addfaf2d336326c5d4adc47a3666d8c52387d7
ifollowyou <ifollowyou@aliyun.com>
2015-09-06 00:35:59 +0800

    update groovy for javaeyes

--- groovy-for-javaeyes/src/main/groovy/org/ifollowyou/tutorial/Annotations.groovy
+++ groovy-for-javaeyes/src/main/groovy/org/ifollowyou/tutorial/Annotations.groovy
@@ -1,12 +1,5 @@
 package org.ifollowyou.tutorial
-/***
- * Excerpted from "Programming Groovy, 2nd Edition",
- * published by The Pragmatic Bookshelf.
- * Copyrights apply to this code. It may not be used to create training material, 
- * courses, books, articles, and the like. Contact us if you are in doubt.
- * We make no guarantees that this code is fit for any purpose. 
- * Visit http://www.pragmaticprogrammer.com/titles/vslg2 for more book information.
-***/
+
 println "//" + "START:CANONICAL_OUTPUT"
 import groovy.transform.*
 
@@ -85,8 +78,8 @@ println "//" + "END:NEWIFY_OUTPUT"
 println "//" + "START:SINGLETON_OUTPUT"
 @Singleton(lazy = true)
 class TheUnique {
-  private TheUnique() { println 'Instance created' }
-  
+//  private TheUnique() { println 'Instance created' }
+
   def hello() { println 'hello' }
 }
 println "Accessing TheUnique"
--- groovy-for-javaeyes/src/main/groovy/org/ifollowyou/tutorial/Greetings.groovy
+++ groovy-for-javaeyes/src/main/groovy/org/ifollowyou/tutorial/Greetings.groovy
@@ -1,19 +1,12 @@
 package org.ifollowyou.tutorial
-/***
- * Excerpted from "Programming Groovy, 2nd Edition",
- * published by The Pragmatic Bookshelf.
- * Copyrights apply to this code. It may not be used to create training material, 
- * courses, books, articles, and the like. Contact us if you are in doubt.
- * We make no guarantees that this code is fit for any purpose. 
- * Visit http://www.pragmaticprogrammer.com/titles/vslg2 for more book information.
-***/
-// Java code
+
+
 public class Greetings {
-  public static void main(String[] args) {
-    for(int i = 0; i < 3; i++) {
-      System.out.print("ho ");
+    public static void main(String[] args) {
+        for (int i = 0; i < 3; i++) {
+            System.out.print("ho ");
+        }
+
+        System.out.println("Merry Groovy!");
     }
-    
-    System.out.println("Merry Groovy!");
-  }
 }
